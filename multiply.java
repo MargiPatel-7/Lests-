de01129880cca9f648f0563291855f3f84c1d24a
Margi Patel <margi.patel@outlook.com>
2023-02-23 14:15:12 +0530

    Added multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply
+{
+    public static void name(String[] args) {
+        int a = 5;
+        int b = 6;
+        Sytem.out.println("The multiplication is:", +(a*b));
+        
+    }
+}
